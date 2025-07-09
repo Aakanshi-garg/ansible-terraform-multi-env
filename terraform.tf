e603f8fe98cb9eb4789c0c50c4b36cd1235d6e80
Aakanshi-garg <145046018+Aakanshi-garg@users.noreply.github.com>
2025-07-10 03:32:31 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "aakanshi-remote-backend"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "aakanshi-remote-backend-table"
+  }
 }
